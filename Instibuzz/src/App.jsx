cc4f99a1ce0df97943ce0638635eff834c236fa2
Rishabh Dotasara <rishabhdotasara@gmail.com>
2024-05-26 09:19:42 +0530

    loading page added!

--- Instibuzz/src/App.jsx
+++ Instibuzz/src/App.jsx
@@ -4,13 +4,16 @@ import { useState } from 'react'
 import './App.css'
 import ThankYou from './components/ThankYou'
 import OurServices from './components/OurServices'
+import Loading from './components/loading/Loading.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <ThankYou/>
+      {/* <ThankYou/> */}
+      <Loading/>
       {/* <OurServicev ,./s/> */}    </>
   )
 
---
+++ Instibuzz/src/components/loading/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./loading.css"
+
+export default function Loading() {
+  return (
+    <div className="loading-cont">
+      <img
+        src="https://www.instibuzz.com/static/media/973300f3-c585-48d9-9e8c-601a3ae24121.07719972837fc4f4e7d8.png"
+        alt="Logo"
+        className="logo"
+      />
+    </div>
+  );
+}
